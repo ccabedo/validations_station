c35680ca8e4d5cd3fb4e9bdcb4d9609d148c3b3f
Colleen Cabedo <cmcabedo@gmail.com>
2022-11-28 11:45:20 -0500

    a

--- app/models/platform.rb
+++ app/models/platform.rb
@@ -3,4 +3,8 @@ class Platform < ApplicationRecord
   belongs_to :station
 
   validates :platform_num, numericality: { only_integer: true }
+  validates :platform_num, inclusion: {in: 1..20}
+  validates :platform_num, uniqueness: true
+
+
 end
